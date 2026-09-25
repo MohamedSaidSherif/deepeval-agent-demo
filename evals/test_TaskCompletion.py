5682c29c11d77508703c9fd11f09e2b0e282aa4b
Mohamed Sherif <mbs0623@dexcom.com>
2026-09-25 14:22:34 +0300

    Initial commit

---
+++ evals/test_TaskCompletion.py
@@ -0,0 +1,20 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from deepeval.evaluate import evaluate
+from deepeval.metrics import TaskCompletionMetric
+from deepeval.test_case import LLMTestCase
+
+from agent_instrumented import support_agent
+
+actual_output = support_agent("Where is my order ORD-1042?")
+
+test_case = LLMTestCase(
+    input = "Where is my order ORD-1042?",
+    actual_output = actual_output
+)
+
+evaluate(test_cases = [test_case],
+         metrics= [TaskCompletionMetric(threshold=0.7,model = "gpt-oss")])
